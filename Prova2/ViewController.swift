a35f5b0cf038428d147098f342db98c0552ba5e1
Luca Lucchesi <lucalucchesi@iMac-di-Luca-Lucchesi.local>
2014-10-29 15:05:43 +0100

    Initial Commit

---
+++ Prova2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prova2
+//
+//  Created by Luca Lucchesi on 29/10/14.
+//  Copyright (c) 2014 Luca Lucchesi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
